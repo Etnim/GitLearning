80325f7ee27e62c994f215f36404389162225a5b
Etnim <87998276+Etnim@users.noreply.github.com>
2024-03-13 16:23:06 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("This is from feature branch, hello!");
-
-        System.out.println("Here is a conflict");
+        System.out.println("Conflicts are deleted");
+        System.out.println("Hello from gitHub");
     }
 }
