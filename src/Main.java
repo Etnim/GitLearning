92a5517a99528a1a738b04ef2660233f2c418aae
Anhelina Velychko <avelichko469@gmail.com>
2024-03-13 16:09:30 +0200

    Added new message 'Hello conflict'

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("This is from feature branch, hello!");
+
         System.out.println("Here is a conflict");
     }
 }
